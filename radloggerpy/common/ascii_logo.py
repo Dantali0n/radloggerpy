6b477380ea033538ce5452ead975137f34ecd0b0
Dantali0n <info@dantalion.nl>
2023-07-08 08:32:49 +0200

    Restore ascii logos

--- radloggerpy/common/ascii_logo.py
+++ radloggerpy/common/ascii_logo.py
@@ -14,40 +14,45 @@
 
 # Disable pep8 for this file
 # flake8: noqa
+# autopep8: off
+# fmt: off
 
 """Logo's to output to terminal when radlogger starts up"""
 
 TEXT = """
-  ____             _  _                                      ____
- |  _ \\  __ _   __| || |     ___    __ _   __ _   ___  _ __ |  _ \\  _   _
- | |_) |/ _` | / _` || |    / _ \\  / _` | / _` | / _ \\| '__|| |_) || | | |
- |  _ <| (_| || (_| || |___| (_) || (_| || (_| ||  __/| |   |  __/ | |_| |
- |_| \\_\\\\__,_| \\__,_||_____|\\___/  \\__, | \\__, | \\___||_|   |_|     \\__, |
-                                   |___/  |___/                     |___/
+  ____             _  _                                      ____          
+ |  _ \  __ _   __| || |     ___    __ _   __ _   ___  _ __ |  _ \  _   _  
+ | |_) |/ _` | / _` || |    / _ \  / _` | / _` | / _ \| '__|| |_) || | | | 
+ |  _ <| (_| || (_| || |___| (_) || (_| || (_| ||  __/| |   |  __/ | |_| | 
+ |_| \_\\\__,_| \__,_||_____|\___/  \__, | \__, | \___||_|   |_|     \__, |
+                                   |___/  |___/                     |___/  
 """
 
 LOGO = """
-               __svnnnnnnnnnns__,
-             )noonvnnnnnnnnnnnoXoo,
-            )vn>   {nvnnnnnnnnnnnoo(
-            )oos, _)nnnnnnnnnnoooow[
-          ajWZ#onnnnnnnnnnnnnoonomZ#6a
-        _jQQQmmmg\"\"\"\"\"\"\"nnnooonXdZZZQQQa
-    __uqWWWWBWBWWhnonoooooonooXZ#Z##QEvns;;..
-  %noSd#ZZZZZZZZZZmonnononoooqZZUZ#ZQkvvvn;;::
- )nvnd#Z#Z#Z#Z#Z#ZZZ2onoooood#Z#Z#ZZQEvnvvn;=:;
+               __svnnnnnnnnnns__,               
+             )noonvnnnnnnnnnnnoXoo,             
+            )vn>   {nvnnnnnnnnnnnoo(            
+            )oos, _)nnnnnnnnnnoooow[            
+          ajWZ#onnnnnnnnnnnnnoonomZ#6a          
+        _jQQQmmmg\"\"\"\"\"\"\"nnnooonXdZZZQQQa 
+    __uqWWWWBWBWWhnonoooooonooXZ#Z##QEvns;;..   
+  %noSd#ZZZZZZZZZZmonnononoooqZZUZ#ZQkvvvn;;::  
+ )nvnd#Z#Z#Z#Z#Z#ZZZ2onoooood#Z#Z#ZZQEvnvvn;=:; 
 )vnn3#ZZ#ZUZ#ZZ#Z#Z#ZnoooooXUZZ#ZZUmQEvvnvns:;;:
 vnnnmXUZUZUZZ#ZUZZ#1owmZ#Zw2nX#ZUZmQ@Ivnvvvv;=:;
 nnnvZZ#Z#ZZ#Z#ZUmm""mmmmmmmmr"mmmQQVvvvvvnvn(;;;
 nnnnnnnnnnnnr`  ....9VVVVVHV(...=:::::;;;:;:;;;;
 nnnnnnnnnnn(  :::::::"nnnv}~::;;;:;;;;;:;;;;;;;;
 )vnnnnnnnno  ::::::::;;::::;=:;::;:;:;:;;;;;;;;.
- {nnnnnonoo .:::::::vvnvvnvn;;:;;;;;;;;;;;;;;=:
-  {nnoooooo .:::::=vvvvnvvnvn<;;:;:;;;;;;;;=;-
-   -"{noooo .::::=vvvvnvvnvvnvs-------------
-            .:::=vnvnvvnvvnvvvvs....
-            .::=nvnvvvnvvnvvnvnvi==.
-            .:)nvnvvnvvnvvnvvnvvvn;.
-             ;::+\"|{vnvvnvvnv\"\"\":;;.
-              ::;;;;;:;;:;;;;;;;;=-
+ {nnnnnonoo .:::::::vvnvvnvn;;:;;;;;;;;;;;;;;=: 
+  {nnoooooo .:::::=vvvvnvvnvn<;;:;:;;;;;;;;=;-  
+   -"{noooo .::::=vvvvnvvnvvnvs-------------    
+            .:::=vnvnvvnvvnvvvvs....            
+            .::=nvnvvvnvvnvvnvnvi==.            
+            .:)nvnvvnvvnvvnvvnvvvn;.            
+             ;::+\"|{vnvvnvvnv\"\"\":;;.        
+              ::;;;;;:;;:;;;;;;;;=-             
                  ---:;;;;;;;:---                """
+
+# fmt: on
+# autopep8: on
